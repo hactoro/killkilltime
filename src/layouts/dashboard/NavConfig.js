fc36d1420c42f297b79ad214e50c8d6cded9714d
hactoro <black1@@4>
2022-04-20 22:43:39 +0900

    edit layout

--- src/layouts/dashboard/NavConfig.js
+++ src/layouts/dashboard/NavConfig.js
@@ -7,40 +7,40 @@ const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
 
 const navConfig = [
   {
-    title: 'dashboard',
+    title: 'MBTI',
     path: '/dashboard/app',
     icon: getIcon('eva:pie-chart-2-fill'),
   },
+  // {
+  //   title: 'user',
+  //   path: '/dashboard/user',
+  //   icon: getIcon('eva:people-fill'),
+  // },
   {
-    title: 'user',
-    path: '/dashboard/user',
-    icon: getIcon('eva:people-fill'),
-  },
-  {
-    title: 'product',
+    title: '퀴즈',
     path: '/dashboard/products',
     icon: getIcon('eva:shopping-bag-fill'),
   },
   {
-    title: 'blog',
+    title: '이상형월드컵',
     path: '/dashboard/blog',
     icon: getIcon('eva:file-text-fill'),
   },
   {
-    title: 'login',
+    title: '로그인',
     path: '/login',
     icon: getIcon('eva:lock-fill'),
   },
   {
-    title: 'register',
+    title: '가입',
     path: '/register',
     icon: getIcon('eva:person-add-fill'),
   },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: getIcon('eva:alert-triangle-fill'),
-  },
+  // {
+  //   title: 'Not found',
+  //   path: '/404',
+  //   icon: getIcon('eva:alert-triangle-fill'),
+  // },
 ];
 
 export default navConfig;
