ef293551c50ed8b15f93151db6c88bbb949c6689
hagrand <hagrand@kbs.co.kr>
2022-04-22 16:49:01 +0900

    Ideal race logic

--- src/_mock/girlIdeals.js
+++ src/_mock/girlIdeals.js
@@ -1,9 +1,21 @@
 const girlIdeals = [
     {
-        src: "https://thumbs.gfycat.com/ScentedLividGoral-mobile.mp4"
+        src: "https://thumbs.gfycat.com/ScentedLividGoral-mobile.mp4",
+        name: "사나"
     },
     {
-        src: "https://thumbs.gfycat.com/VariableShortGoral-mobile.mp4"
+        src: "https://thumbs.gfycat.com/VariableShortGoral-mobile.mp4",
+        name: "아이유"
+    },
+    {
+        src: "https://thumbs.gfycat.com/IncompatibleCloseLamprey-mobile.mp4",
+        name: "트와이스1"
+
+    },
+    {
+        src: "https://thumbs.gfycat.com/CanineUnnaturalAustraliancurlew-mobile.mp4",
+        name: "다현"
+
     }
 ]
 
--- src/sections/@dashboard/Ideal/IdealVs.js
+++ src/sections/@dashboard/Ideal/IdealVs.js
@@ -1,6 +1,6 @@
-import React,{useState, useRef} from 'react';
+import React,{useState, useRef, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import { Stack, Box, Container } from '@mui/material';
+import { Stack, Box, Container, Card, CardContent } from '@mui/material';
 import {styled} from '@mui/material/styles';
 import ReactPlayer from 'react-player';
 import girlIdeals from '../../../_mock/girlIdeals';
@@ -8,41 +8,121 @@ import girlIdeals from '../../../_mock/girlIdeals';
 export default function IdelaVs(){
 
     const { id } = useParams();
-    console.log(id);
-    const imgStyle = styled('img')({
 
+    const Div = styled('div')({
+        background: "yellow",
     })
+    const CardMediaStyle = styled('div')({
+        position: 'relative',
+        // paddingTop: 'calc(100% * 3 / 4)'
+    })
+    const VideoCover = styled('div')({
+        // video tag 의 경우 event 전달이 잘 안돼서 약간의 트릭을 쓴거!
+        position: 'absolute',
+        width: "100%",
+        height: "100%",
+        background: "yellow",
+        opacity: "0",
+        top: "0"
+    })
+    const TitleStyle = styled('div')({
+        textAlign: 'center'
+    })
+
+    const startRound = 4; 
+    const nextRound = useRef((startRound/2));
     const offset = useRef(0);
+    const isFirst = useRef(true);
+    const isFirst2 = useRef(true);
+
+
     const [items, setItems] = useState(girlIdeals);
     const [winners, setWinners] = useState([]);
-    const [round, setRound] = useState([items[offset.current], items[offset.current+1]]);
+    const [players, setPlayers] = useState([items[offset.current], items[offset.current+1]]);
     
+    useEffect(()=>{
+
+        if(isFirst.current) {
+            isFirst.current = false;
+            return;
+        } 
+
+        if( (nextRound.current !== 1) && (winners.length >= nextRound.current)){
+            const toNext = nextRound.current / 2;
+            nextRound.current = toNext;
+            setItems(winners);
+        }else{
+
+            const toOffset = offset.current + 2;
+            offset.current = toOffset;
+            setPlayers(
+                [
+                    items[offset.current],
+                    items[offset.current+1]
+                ]
+            )
+        }
+    },[winners])
+
+    useEffect(()=>{
+
+        if(isFirst2.current) {
+            isFirst2.current = false;
+            return;
+        }
+        offset.current = 0;
+
+        setPlayers(
+            [
+                items[offset.current],
+                items[offset.current+1]
+            ]
+        )
+    },[items])
 
     const roundUpHandler = (e) => {
-    }
-    // setRound([
-    //     items[0],
-    //     items[1]
-    // ])
 
-    // console.log(round);
+        if(nextRound.current === 1){
+            // game end
+            // go winner page
+            alert(`winner is ${players[e.target.className[0]].name}`);
+            return;
+        }
+        setWinners([
+            ...winners,
+            players[e.target.className[0]]
+        ]);
+        
+    }
+
     return(
 
         <Container sx={{border:"1px solid black"}}>
-            <Stack direction="row" alignItems="center" justifyContent="center" sx={{border:"1px solid black"}}>
-                {round.map((item, index)=>{
+            <Stack direction="row" alignItems="center" justifyContent="center" stretch sx={{border:"1px solid black"}}>
+                {players.map((item, index)=>{
                     return(
-                    <Box onClick={roundUpHandler}>
-                        <ReactPlayer
-                            url={item.src}
-                            width="100%"
-                            height="100%"
-                            muted
-                            playing
-                            playsinline
-                            loop
-                            />
-                    </Box>
+                        <Card onClick={roundUpHandler}>
+                            <CardMediaStyle>
+                                <video 
+                                    width="100%" 
+                                    height="100%"
+                                    muted
+                                    autoPlay
+                                    loop
+                                    playinline >
+                                    <source src={item.src} />
+                                </video>
+                                <VideoCover className={index}>
+                                    imageCover
+                                </VideoCover>
+                            </CardMediaStyle>
+                            <CardContent>
+                                <TitleStyle >                            
+                                    {item.name}
+                                </TitleStyle>
+                            </CardContent>
+                        </Card>
+                
                     )
                 })}
         
